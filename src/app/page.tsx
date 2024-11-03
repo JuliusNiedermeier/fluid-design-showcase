b9793289c08f0041fc6be1b46987972a5c814d06
JuliusNiedermeier <julius.niedermeier@outlook.de>
2024-11-03 16:08:38 +0100

    Added some more showcase elements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import {
   ArrowRightIcon,
   ComponentIcon,
+  CopyrightIcon,
   FullscreenIcon,
   MonitorIcon,
+  MoveHorizontalIcon,
   PlayIcon,
   SmartphoneIcon,
   TabletIcon,
@@ -16,14 +18,17 @@ export default function Home() {
   return (
     <>
       <div className="mx-16 border-x p-8">
-        <div className="flex gap-1 items-end">
-          <div className="aspect-square w-4 mb-px bg-blue-600" />
-          <span className="text-xl font-bold leading-none">21</span>
-          <span className="opacity-50 leading-none">Studio</span>
+        <div className="flex gap-1 items-center">
+          <span className="font-medium">Fluid Design</span>
+          <span className="opacity-50">Showcase</span>
+          <div className="ml-auto flex py-2 px-4 items-center gap-2 bg-blue-700 w-fit rounded-full">
+            <span className="text-blue-100 text-sm font-medium">Resize viewport</span>
+            <MoveHorizontalIcon className="size-4 text-blue-100" />
+          </div>
         </div>
       </div>
       <div className="border-t px-16">
-        <div className="border-x grid grid-cols-3">
+        <div className="border-x grid grid-cols-3 min-h-[35rem]">
           <div className="p-8 border-r flex flex-col gap-4 bg-violet-100 tablet:bg-emerald-100 desktop:bg-orange-100">
             <div className="hidden desktop:flex py-2 px-4 items-center gap-2 bg-neutral-900 w-fit rounded-full ring-[0.25rem] ring-neutral-900/10">
               <MonitorIcon className="size-4 text-neutral-100" />
@@ -46,21 +51,25 @@ export default function Home() {
               </span>
             </div>
 
-            <h1 className="text-3xl font-bold mt-auto">Fluid responsive</h1>
-            <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore,
-              quasi unde harum consequuntur explicabo, blanditiis tempora dolor
-              recusandae beatae eum nihil quam earum sapiente error, eius
-              perspiciatis obcaecati autem rem.
-            </p>
+            <div className="mt-auto pt-16">
+              <h1 className="text-[clamp(2rem,1vw,4rem)] font-bold">
+                Fluid responsive
+              </h1>
+              <p className="mt-2">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Dolore, quasi unde harum consequuntur explicabo, blanditiis
+                tempora dolor recusandae beatae eum nihil quam earum sapiente
+                error, eius perspiciatis obcaecati autem rem.
+              </p>
+            </div>
           </div>
           <div className="p-8 col-span-2 group hover:bg-neutral-100 saturate-50 hover:saturate-100 transition-all cursor-pointer">
-            <div className="relative bg-neutral-200 aspect-video grid place-content-center">
+            <div className="relative bg-neutral-200 h-full rounded-xl overflow-hidden grid place-content-center">
               <Image
                 src={graphicImage}
                 fill
                 alt=""
-                className="object-cover rounded-xl"
+                className="object-cover"
                 placeholder="blur"
               />
               <div className="p-8 rounded-full bg-white relative group-hover:bg-black group-hover:text-white">
@@ -129,8 +138,11 @@ export default function Home() {
         </div>
       </div>
       <div className="border-t px-16">
-        <div className="border-x p-8">
-          <span>All rights reserved.</span>
+        <div className="border-x p-8 flex gap-4 items-center">
+          <CopyrightIcon className="size-4" />
+          <span className="text-sm font-medium">
+            All rights reserved by Julius Niedermeier
+          </span>
         </div>
       </div>
     </>
